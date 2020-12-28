edfb97afbdc7c41e3c267d69405af96aae3f8836
Oleksandr Ihnatenko <ihnatenko@gmail.com>
2020-12-28 18:15:43 +0200

    Added explanations for import order and for separate regex's

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ module.exports = {
   arrowParens: "always",
   endOfLine: "lf",
   printWidth: 85,
-  importOrder: ["^@/(.*)$", "^(?=.*\\.\/)(?:(?!scss|css).)*$", "\\w.*(css|scss)+.*"],
+  // Array below used for @trivago/prettier-plugin-sort-imports
+  // to specify import types order, left to 
+  // right sorted in file as top to bottom,
+  // on the very top will be placed imports
+  // which can`t be found in this array as they
+  // considered as node modules imports.
+  //
+  // regex explanations left to right:
+  // 1: Takes imports which begins with "@/" signs;
+  // 2: Takes imports which begins with "./" and NOT ends with "css" or "scss";
+  // 3: Takes imports which begins with "./" AND ends with "css" or "scss";
+  importOrder: ["^@/(.*)$", "^(?=.*\\.\/)(?:(?!scss|css).)*$", "^\\.\/.*(css|scss)"],
   importOrderSeparation: true,
 };
